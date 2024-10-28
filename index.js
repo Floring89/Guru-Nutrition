6ea8493d04927a14fb95ecefef92a0877ded2921
Floring89 <floringarcineanu89@gmail.com>
2024-10-28 19:52:48 +0200

    add: save cart to local storage

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import { getAllProducts } from "./api/alimente.js";
 import { mapProductToCard } from "./utils/layout.js";
 
-document.addEventListener('DOMContentLoaded', displayProducts);
+document.addEventListener("DOMContentLoaded", displayProducts);
 
 async function displayProducts() {
-    const products = await getAllProducts();
-    document.querySelector('.products').innerHTML = products
+  const products = await getAllProducts();
+  document.querySelector(".products").innerHTML = products
     .map(mapProductToCard)
-    .join('');
+    .join("");
 
+  const addToCartButtons = document.querySelectorAll(".add-to-cart");
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.getAttribute("data-productId");
+      let cart = JSON.parse(localStorage.getItem("cart")) || {};
+
+      if (cart[productId]) {
+        cart[productId].quantity++;
+      } else {
+        cart[productId] = { quantity: 1 };
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+  });
 }
